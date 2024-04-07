dc0054b8c35e5c847d62b7047c9879638f94861d
zcqing <jeseeqing@gmail.com>
2024-04-07 13:38:20 +0800

    extract Pagination struct from TableData

--- controller/table_data.go
+++ controller/table_data.go
@@ -1,18 +1,25 @@
 package controller
 
 type TableData struct {
-	List        interface{} `json:"list"`
-	Total       int64       `json:"total"`
-	PageSize    int         `json:"pageSize"`
-	CurrentPage int         `json:"currentPage"`
+	List  interface{} `json:"list"`
+	Total int64       `json:"total"`
+
+	Pagination
+}
+
+type Pagination struct {
+	PageSize    int `json:"pageSize"`
+	CurrentPage int `json:"currentPage"`
 }
 
 func newDefaultTableData(list interface{}, total int64) *TableData {
 	return &TableData{
-		List:        list,
-		Total:       total,
-		PageSize:    10,
-		CurrentPage: 1,
+		List:  list,
+		Total: total,
+		Pagination: Pagination{
+			PageSize:    10,
+			CurrentPage: 1,
+		},
 	}
 }
 
